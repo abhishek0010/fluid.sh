543c64cebb564f38b8ae34d98ec8758adf27a510
aspectrr <collin@steelbrowser.com>
2026-01-06 08:42:39 -0500

    fix: remove .env, update sdk

--- sdk/virsh-sandbox-py/virsh_sandbox/api/access_api.py
+++ sdk/virsh-sandbox-py/virsh_sandbox/api/access_api.py
@@ -7,9 +7,6 @@
 
 from typing import Any, Dict, List, Optional, Tuple, Union
 
-from pydantic import Field, StrictBool, StrictInt, StrictStr
-from typing_extensions import Annotated
-
 from virsh_sandbox.api_client import ApiClient, RequestSerialized
 from virsh_sandbox.api_response import ApiResponse
 from virsh_sandbox.exceptions import ApiException
